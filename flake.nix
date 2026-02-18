c2ded8e83d8c979a030da72da43dbab035e2d71f
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2026-02-18 16:32:44 +0300

    feat: add flakes structure

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+	description = "Zapolyarny's NixOS config";
+	
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		home-manager.url = "github:nix-community/home-manager";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	outputs = { self, nixpkgs, home-manager, ... }:
+	let
+		system = "x86_64-linux"l
+	in {
+		nixosConfigurations.zapolyarny-x380 = nixpkgs.lib.nixosSystem {
+			inherit system;
+			
+			modules = [
+				./hosts/zapolyarny-x380/configuration.nix
+				./hardware-configuration.nix
+
+				home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.zapolyarny = import ./home/zapolyarny.nix;
+				}
+			];
+		};
+	};
+}
---
+++ home/zapolyarny.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+	home.username = "zapolyarny";
+	home.homeDirectory = "/home/zapolyarny";
+	home.stateVersion = "25.11";
+}
