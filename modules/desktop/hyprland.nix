ff396d35b539fbdd3c486064d8ffa57675d22127
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2026-03-01 19:43:51 +0300

    feat(greetd): configure tuigreet theme and session

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -6,10 +6,42 @@
   programs.dconf.enable = true;
   programs.hyprland.enable = true;
 
-  services.greetd.enable = true;
-  services.greetd.settings.default_session = {
-     command = "${pkgs.tuigreet}/bin/tuigreet --cmd ${pkgs.hyprland}/bin/Hyprland";
+  services.greetd = {
+    enable = true;
+
+    settings = {
+	default_session = {
+		command = "${pkgs.greetd.tuigreet}/bin/tuigreet \
+			--time \
+			--remember \
+			--remember-user-session \
+			--asterisks \
+			--theme border=magenta;text=white;prompt=magenta;time=cyan;action=magenta \
+			--cmd Hyprland";
+			user = "greeter";
+		
+      };
+    };
   };
+
+  console.colors = [
+	"1e1e2e"
+	"f38ba8"
+	"a6adc8"
+	"f5c2e7"
+	"cdd6f4"
+	"89b4fa"
+	"94e2d5"
+	"bac2de"
+	"585b70"
+	"f38ba8"
+	"a6adc8"
+	"f5c2e7"
+	"cdd6f4"
+	"89b4fa"
+	"94e2d5"
+	"ffffff"
+  ];
 
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [
