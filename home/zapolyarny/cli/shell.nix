44befbb98444f28a26fff6ba56fbce4abd1c0d91
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2026-05-01 15:59:49 +0300

    fix(home): configure zsh aliases

--- home/zapolyarny/cli/shell.nix
+++ home/zapolyarny/cli/shell.nix
@@ -1,10 +1,17 @@
 {
-  home.shellAliases = {
-    ls = "eza";
-    ll = "eza -lh";
-    la = "eza -lah";
-    tree = "eza --tree";
-    cat = "bat";
-    grep = "rg";
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -lh";
+      la = "eza -lah";
+      tree = "eza --tree";
+      cat = "bat";
+      grep = "rg";
+    };
   };
 }
