fa6ba1e8c2c59afdc7fe88f3b4a1242dd6a62d3f
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2026-05-01 23:59:41 +0300

    feat(nix): harden nix daemon defaults

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,10 +1,46 @@
 _:
 
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    channel.enable = false;
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    optimise = {
+      automatic = true;
+      dates = [ "weekly" ];
+    };
+
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+
+      min-free = 5 * 1024 * 1024 * 1024;
+      max-free = 20 * 1024 * 1024 * 1024;
+
+      max-jobs = "auto";
+      sandbox = true;
+      keep-going = true;
+      log-lines = 30;
+
+      warn-dirty = false;
+      accept-flake-config = false;
+
+      keep-derivations = true;
+      keep-outputs = true;
+    };
+  };
 
   system.stateVersion = "25.11";
 }
